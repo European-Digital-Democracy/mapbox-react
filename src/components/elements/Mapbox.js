8ef92e55dbf9a93b3e1faba7f4192b5f432622d1
peter westman <pwestman@gmail.com>
2020-08-29 22:51:39 +0200

    add tileset

--- src/components/elements/Mapbox.js
+++ src/components/elements/Mapbox.js
@@ -3,20 +3,20 @@ import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
 const Map = ReactMapboxGl({
     accessToken:
-        'pk.eyJ1IjoicHdlc3RtYW4iLCJhIjoiY2s4MGQ2NGFuMGVrMDNkcnVwdWxtZGp3cSJ9.ZjCCygV_IlZ9MvMbafseBQ'
+        'pk.eyJ1IjoicHdlc3RtYW4iLCJhIjoiY2tlZmxhaG4wMWRsYzJzbWlpZTVhbWl1eCJ9.ZqGDwQ8qHtXLgNY4tWIFMg'
 });
 
 function Mapbox() {
     return (
         <Map
-            style="mapbox://styles/pwestman/ckefir9hm1ww319lzg43rtn4t"
+            style="mapbox://styles/pwestman/ckeg4udh62iai19qsevf51cax"
             containerStyle={{
                 height: '70vh',
-                width: '100vw',
+                width: '90vw',
             }}
         >
             <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-                <Feature coordinates={[2.35, 51.3233379650232]} />
+                <Feature coordinates={[5.35, 51.3233379650232]} />
             </Layer>
         </Map>
     )
