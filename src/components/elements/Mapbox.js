3a3f8ccc1266585f7654ad40e59f11d589d5853e
peter westman <pwestman@gmail.com>
2020-08-29 23:42:25 +0200

    change map

--- src/components/elements/Mapbox.js
+++ src/components/elements/Mapbox.js
@@ -3,21 +3,18 @@ import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
 const Map = ReactMapboxGl({
     accessToken:
-        'pk.eyJ1IjoicHdlc3RtYW4iLCJhIjoiY2tlZmxhaG4wMWRsYzJzbWlpZTVhbWl1eCJ9.ZqGDwQ8qHtXLgNY4tWIFMg'
+        'pk.eyJ1IjoicHdlc3RtYW4iLCJhIjoiY2tlZmxhaG4wMWRsYzJzbWlpZTVhbWl1eCJ9.ZqGDwQ8qHtXLgNY4tWIFMg',
 });
 
 function Mapbox() {
     return (
         <Map
-            style="mapbox://styles/pwestman/ckeg5fy4w2irx19nt12mn7ox4"
+            style="mapbox://styles/pwestman/ckeg6d7d62jod19lz0lczn8mm"
             containerStyle={{
                 height: '70vh',
                 width: '90vw',
             }}
         >
-            <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-                <Feature coordinates={[5.35, 51.3233379650232]} />
-            </Layer>
         </Map>
     )
 }
