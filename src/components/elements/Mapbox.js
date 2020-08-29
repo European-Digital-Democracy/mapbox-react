538eaa72bb65ac3cccc0f00c1dfa4840c79f6c40
peter westman <pwestman@gmail.com>
2020-08-29 13:34:53 +0200

    enlarge map

--- src/components/elements/Mapbox.js
+++ src/components/elements/Mapbox.js
@@ -9,14 +9,14 @@ const Map = ReactMapboxGl({
 function Mapbox() {
     return (
         <Map
-            style="mapbox://styles/mapbox/streets-v9"
+            style="mapbox://styles/pwestman/ckefir9hm1ww319lzg43rtn4t"
             containerStyle={{
-                height: '50vh',
-                width: '50vw'
+                height: '70vh',
+                width: '100vw',
             }}
         >
             <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+                <Feature coordinates={[2.35, 51.3233379650232]} />
             </Layer>
         </Map>
     )
