27633b469d135d6053e91da9376cb98581dc1eb5
peter westman <pwestman@gmail.com>
2020-09-30 15:02:12 +0200

    update tokens

--- src/components/elements/Mapbox.js
+++ src/components/elements/Mapbox.js
@@ -3,13 +3,13 @@ import ReactMapboxGl from 'react-mapbox-gl';
 
 const Map = ReactMapboxGl({
     accessToken:
-        'pk.eyJ1IjoicHdlc3RtYW4iLCJhIjoiY2tlZmxhaG4wMWRsYzJzbWlpZTVhbWl1eCJ9.ZqGDwQ8qHtXLgNY4tWIFMg',
+        'pk.eyJ1IjoicHdlc3RtYW4iLCJhIjoiY2tmaTd5eHJjMDBuZjJ6cDdoMW92bGdzYSJ9.ku-wJSzHoysxwRbRb6Jq7Q',
 });
 
 function Mapbox() {
     return (
         <Map
-        style={"mapbox://styles/pwestman/ckeg6d7d62jod19lz0lczn8mm"}
+            style={"mapbox://styles/pwestman/ckeg5fy4w2irx19nt12mn7ox4"}
         zoom={[3.8]}
         center={[18.37, 50.86]}
         containerStyle={{
